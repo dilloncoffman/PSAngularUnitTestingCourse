beb870e0543aaf6fd940ab496c977f10c6af5fc2
Dillon Coffman <tug40394@temple.edu>
2020-06-03 10:38:21 -0400

    Shallow integration tests - created mock child component to avoid having to use NO_ERRORS_SCHEMA

--- src/app/heroes/heroes.component.shallow.spec.ts
+++ src/app/heroes/heroes.component.shallow.spec.ts
@@ -2,12 +2,23 @@ import { ComponentFixture, TestBed } from "@angular/core/testing"
 import { HeroesComponent } from "./heroes.component"
 import { HeroService } from "../hero.service";
 import { of } from "rxjs/internal/observable/of";
-import { NO_ERRORS_SCHEMA } from "@angular/core";
+import { NO_ERRORS_SCHEMA, Component, Input, Output, EventEmitter } from "@angular/core";
+import { Hero } from "../hero";
+import { By } from "@angular/platform-browser";
 
 describe('HeroesComponent (shallow tests)', () => {
   let fixture: ComponentFixture<HeroesComponent>;
   let mockHeroService;
   let HEROES;
+
+  @Component({
+    selector: 'app-hero',
+    template: '<div></div>',
+  })
+  class MockHeroComponent {
+    @Input() hero: Hero;
+    // @Output() delete = new EventEmitter();
+  }
 
   beforeEach(() => {
     HEROES = [{ id: 1, name: 'Batman', strength: 8 }, { id: 2, name: 'Superman', strength: 24 }, { id: 3, name: 'Deadpool', strength: 15 }];
@@ -15,11 +26,10 @@ describe('HeroesComponent (shallow tests)', () => {
     mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero']); // creates a mock service with array of methods
 
     TestBed.configureTestingModule({
-      declarations: [HeroesComponent],
+      declarations: [HeroesComponent, MockHeroComponent],
       providers: [
         { provide: HeroService, useValue: mockHeroService } // when anyone asks for a HeroService, use the mockHeroService
       ],
-      schemas: [NO_ERRORS_SCHEMA]
     })
     fixture = TestBed.createComponent(HeroesComponent);
   });
@@ -31,5 +41,13 @@ describe('HeroesComponent (shallow tests)', () => {
     fixture.detectChanges();
 
     expect(fixture.componentInstance.heroes.length).toBe(3);
+  });
+
+  // Count DOM elements produced for each hero in HEROES
+  it('should create one li for each hero', () => {
+    mockHeroService.getHeroes.and.returnValue(of(HEROES));
+    fixture.detectChanges();
+
+    expect(fixture.debugElement.queryAll(By.css('li')).length).toBe(3);
   })
 })
