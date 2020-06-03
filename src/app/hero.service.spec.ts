0d666d0ba8a4917420f6f74e7021711c32583e0e
Dillon Coffman <tug40394@temple.edu>
2020-06-03 15:12:21 -0400

    Deep integration tests - mocked http request and response; 1. get a handle to service (mock other services if required by the service you are calling) 2. make call to service 3. use httpTestingController to expect what API URL should have been hit by the service and store that in a req object 4. use req object to flush() back mock data you'd expect to have been returned 5. mock data from req.flush() is available inside the service call's subscription from (2.)

--- src/app/hero.service.spec.ts
+++ src/app/hero.service.spec.ts
@@ -1,11 +1,13 @@
-import { TestBed } from "@angular/core/testing"
+import { TestBed, inject } from "@angular/core/testing"
 import { HeroService } from "./hero.service"
 import { MessageService } from "./message.service";
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
+import { Hero } from "./hero";
 
 describe('HeroService', () => {
   let mockMessageService;
   let httpTestingController: HttpTestingController;
+  let service: HeroService;
 
   beforeEach(() => {
     mockMessageService = jasmine.createSpyObj(['add']);
@@ -17,7 +19,20 @@ describe('HeroService', () => {
       imports: [HttpClientTestingModule]
     });
     httpTestingController = TestBed.get(HttpTestingController); // .get() looks inside dependency injection registry for this TestBed's module and finds the service that correlates to that type and gives us a handle to it
+
+    service = TestBed.get(HeroService);
   });
+  describe('getHero', () => {
+    it('should call get with the correct URL', () => {
+      service.getHero(4).subscribe(() => {
+        // won't execute until flush() is executed below 
+        // where data from flush({mock data to return}) is returned
+      }); // make the call to the service
 
+      const req = httpTestingController.expectOne('api/heroes/4'); // notice we expect this call, after we've actually made the call above
+      req.flush({ id: 4, name: 'SuperDude', strength: 100 }); // the data that will be sent back when the request at the specified URL is hit in the actual HeroService
 
+      httpTestingController.verify(); // if two calls to the service were made when we only expectOne() then this ensures the test will fail
+    })
+  });
 });
