b3b23e5bbb49c6cd2d870c595120f29eb8c0ba84
Dillon Coffman <tug40394@temple.edu>
2020-06-03 11:21:55 -0400

    Set up integration test for testing Services

---
+++ src/app/hero.service.spec.ts
@@ -0,0 +1,22 @@
+import { TestBed } from "@angular/core/testing"
+import { HeroService } from "./hero.service"
+import { MessageService } from "./message.service";
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
+
+describe('HeroService', () => {
+  let mockMessageService;
+  let httpTestingController: HttpTestingController;
+
+  beforeEach(() => {
+    mockMessageService = jasmine.createSpyObj(['add']);
+    TestBed.configureTestingModule({
+      providers: [
+        HeroService,
+        { provide: MessageService, useValue: mockMessageService }
+      ],
+      imports: [HttpClientTestingModule]
+    })
+  });
+
+  httpTestingController = TestBed.get(HttpTestingController); // .get() looks inside dependency injection registry for this TestBed's module and finds the service that correlates to that type and gives us a handle to it
+});
