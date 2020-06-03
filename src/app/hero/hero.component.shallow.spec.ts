f46aec5039ba1e763bdf06fa6bde02e149d47ac8
Dillon Coffman <tug40394@temple.edu>
2020-06-03 09:31:33 -0400

    Shallow integration tests - using fixture.nativeElement and fixture.debugElement

--- src/app/hero/hero.component.shallow.spec.ts
+++ src/app/hero/hero.component.shallow.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, ComponentFixture } from "@angular/core/testing"
 import { HeroComponent } from "./hero.component"
 import { NO_ERRORS_SCHEMA } from "@angular/core";
+import { By } from "@angular/platform-browser";
 
 describe('HeroComponent (shallow tests)', () => {
   let fixture: ComponentFixture<HeroComponent>; // wrapper for component with a type to get intellisense support
@@ -21,5 +22,15 @@ describe('HeroComponent (shallow tests)', () => {
     fixture.componentInstance.hero = { id: 1, name: 'Batman', strength: 10 };
 
     expect(fixture.componentInstance.hero.name).toEqual('Batman')
+  })
+
+  it('should render the hero name', () => {
+    fixture.componentInstance.hero = { id: 1, name: 'Batman', strength: 10 };
+
+    fixture.detectChanges(); // execute change detection to update any bindings like the hero.name and hero.id properties on the component fixture, otherwise this test will fail
+
+    // Both expect() assertions are equal - one using debugElement and the the other using nativeElement
+    expect(fixture.debugElement.query(By.css('a')).nativeElement.textContent).toContain('Batman')
+    // expect(fixture.nativeElement.querySelector('a').textContent).toContain('Batman')
   })
 })
