edf27e647727aff00c8c57f6a7607b9220c20154
Dillon Coffman <tug40394@temple.edu>
2020-06-04 08:21:49 -0400

    Dealt with ngModel test error by importing FormsModule into beforeEach() of TestBed.configureTestingModule({})a

--- src/app/hero-detail/hero-detail.component.spec.ts
+++ src/app/hero-detail/hero-detail.component.spec.ts
@@ -1,20 +1,24 @@
-import { TestBed } from "@angular/core/testing"
+import { TestBed, ComponentFixture } from "@angular/core/testing"
 import { HeroDetailComponent } from "./hero-detail.component"
 import { ActivatedRoute } from "@angular/router";
 import { HeroService } from "../hero.service";
 import { Location } from '@angular/common'; // Location global var for browser
+import { of } from "rxjs/internal/observable/of";
+import { FormsModule } from "@angular/forms";
 
 describe('HeroDetailComponent', () => {
-  let fixture, mockActivatedRoute, mockHeroService, mockLocation;
+  let fixture: ComponentFixture<HeroDetailComponent>; // allows for intellisense
+  let mockActivatedRoute, mockHeroService, mockLocation;
 
   beforeEach(() => {
     mockActivatedRoute = { // don't need to use jasmine spy to mock in this case, can just hand code
       snapshot: { paramMap: { get: () => { return '3'; } } }
     }
-    mockHeroService = jasmine.createSpyObj('getHero', 'updateHero'); // pass in methods used in this test case
+    mockHeroService = jasmine.createSpyObj(['getHero', 'updateHero']); // pass in methods used in this test case
     mockLocation = jasmine.createSpyObj(['back']);
 
     TestBed.configureTestingModule({
+      imports: [FormsModule],
       declarations: [HeroDetailComponent],
       providers: [
         { provide: ActivatedRoute, useValue: mockActivatedRoute },
@@ -24,7 +28,12 @@ describe('HeroDetailComponent', () => {
     });
 
     fixture = TestBed.createComponent(HeroDetailComponent);
+    mockHeroService.getHero.and.returnValue(of({ id: 3, name: 'Superman', strength: 100 }))
   });
 
+  it('should render the hero name in an h2 tag', () => {
+    fixture.detectChanges(); // gets updated mockHeroService Superman data by forcing change detection
 
+    expect(fixture.nativeElement.querySelector('h2').textContent).toContain('SUPERMAN'); // since we pipe it to uppercase in the template itself
+  });
 });
